b10e3f60f31c2ef326df2b556f0e6c05905e271e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2023-10-18 21:08:16 +0900

    ローカル開発できるところまで

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    https: {
+      key: fs.readFileSync("./localhost-key.pem"),
+      cert: fs.readFileSync("./localhost.pem"),
+    },
+  },
   plugins: [react()],
-})
+  base: "./",
+});
